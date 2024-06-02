415fa94293212f1850b258ed849140141c4b4e84
Zannatul Ferdousi Hema <shanzidadisha1234@gmail.com>
2024-06-03 03:16:29 +0600

    error commit

--- index.js
+++ index.js
@@ -29,6 +29,8 @@ const client = new MongoClient(uri, {
 
 async function run() {
     try {
+
+        await client.connect();
         const database = client.db("Antopolis");
         const animalsDB = database.collection("animals");
         const catagoriesDB = database.collection("catagories");
