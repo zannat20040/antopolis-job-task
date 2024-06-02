72a5ebd40a2f6518650547157583e1680af0f913
Zannatul Ferdousi Hema <shanzidadisha1234@gmail.com>
2024-06-03 03:47:45 +0600

    cors change

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://antopolis-client.vercel.app',
     credentials: true
 }));
 
